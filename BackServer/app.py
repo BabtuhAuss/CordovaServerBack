32ca48b805e3c2925c342657f258daf39a17c052
Baptiste Aussenac <aussenacb@gmail.com>
2019-09-18 04:42:36 +0200

    initialisation serveur flask ( avec le venv pour voir si c'est plus simple)

---
+++ BackServer/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from flask_jsonpify import jsonify
+from flask_restful import Resource, Api
+from json import dumps
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/test', methods=['POST'])
+def add_tag():
+    content = request.json
+    id_annotation = content['id']
+    return jsonify({"Server":"Added : "+id_annotation})
+
+
+
+if __name__ == '__main__':
+    app.run()
