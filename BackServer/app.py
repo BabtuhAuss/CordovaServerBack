56cd81d5b199471856966b14cbfb06c7f72e6eb0
Baptiste Aussenac <aussenacb@gmail.com>
2019-09-20 16:43:03 +0200

    init project +

--- BackServer/app.py
+++ BackServer/app.py
@@ -4,19 +4,22 @@ from flask_restful import Resource, Api
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def init():
+    #appelle a l'initialisation de la db
+    print("appel main db")
 
 @app.route('/test', methods=['POST'])
 def add_tag():
+    #ouverture connection db
     content = request.json
     id_annotation = content['id']
+    #traitement requete base de données et renvoie en json les informations
+
+
+    #fermeture connection db
     return jsonify({"Server":"Added : "+id_annotation})
 
-
+init()
 
 if __name__ == '__main__':
     app.run()
---
+++ BackServer/initDB.py
@@ -0,0 +1,9 @@
+#initialisation DB qui sera appelé à chaque fois que tu lances le serveur :
+"""- suppression des tables existantes
+- création des tables
+- remplissage
+"""
+
+def main():
+    #d
+    print("a")
